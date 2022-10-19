5dd422d1f0b3dbe344f5d2312cb7b76aa2895831
maheshwar4444 <103100700+maheshwar4444@users.noreply.github.com>
2022-10-19 12:19:14 +0530

    add practice project

---
+++ Practice/src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+
+
+let firstname ='Mahesh';
+let lastname = 'Mandale';
+let beltColour ='Orange';
+
+	$: fullname = {firstname} ,{lastname}
+	$: console.log (beltColour)
+
+
+	const handleClick=()=>{
+		beltColour='Black';
+	};
+
+	const handleInput=(e) => {
+		beltColour= e.target.value
+	};
+
+</script>
+
+<main>
+	
+	<p>{fullname} - {beltColour} belt</p>
+	
+	<!-- <button on:click={handleClick}>Update BeltColour</button> -->
+	<!-- <input type="text" on:input={handleInput} value={beltColour}> -->
+	<input type="text" bind:value={firstname}>
+	<input type="text" bind:value={lastname}>
+	<input type="text" bind:value={beltColour}>
+</main>
+
+<style>
+main{
+	text-align: center;
+}
+</style>
